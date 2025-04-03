bc3372e348060090b21d60e6e9ceb1249d71575f
Dnilson Achahuanco Huarilloclla <dnilson.achahuanco@tecsup.edu.pe>
2025-04-03 13:43:58 -0500

    Configuración Inicial en MainActivity.kt

--- app/src/main/java/com/tecsup/bienvenidoalcurso/MainActivity.kt
+++ app/src/main/java/com/tecsup/bienvenidoalcurso/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,13 +19,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BienvenidoAlCursoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MateralTheme.colorScheme.background
+            ){
+                GreetingCard()
             }
         }
     }
